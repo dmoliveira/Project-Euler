a8e5eed239f68ea7a5fae4e0cbf096198a7c18f3
dm.oliveira <dmztheone@gmail.com>
2016-01-01 18:44:01 -0200

    Got a more sofisticated solution for largest prime factor.

--- problems/p03_largest_prime_factor.jl
+++ problems/p03_largest_prime_factor.jl
@@ -1,18 +1,10 @@
 #!/usr/bin/env julia
 
 # Parameters
-N = length(ARGS) > 0 ? parse(Int, ARGS[1]) : 600_851_475_143
+N = 600_851_475_143
 
 # Solution
-largest_prime_factor = -1
-x = N-1
-while x > 1
-    if N % x == 0 && isprime(x) 
-        largest_prime_factor = x
-        break
-    end
-    x -= 1
-end
+largest_prime_factor = sort(collect(keys(factor(N))))[end]
 
 # Answer
 println("The largest prime factor for $N is $largest_prime_factor")
